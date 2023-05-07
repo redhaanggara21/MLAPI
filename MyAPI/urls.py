3fbaa8d0956140af90337bd87b65355368cf1f38
Redha Bayu Anggara <redhabayuanggara@gmail.com>
2023-05-07 20:49:08 +0700

    save

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -1,15 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
+from rest_framework import routers
 
-# , include
-# from . import views
-# from rest_framework import routers
-
-# router = routers.DefaultRouter()
-# router.register('MyAPI', views.ApprovalsView)
-
+router = routers.DefaultRouter()
+router.register('MyAPI', views.ApprovalsView)
 urlpatterns = [
-    # path('form/', views.myform, name='myform'),
-    # path('api/', include(router.urls)),
-    # path('status/', views.approvereject),
- 
-]
+	path('form/', views.myform, name='myform'),
+    path('api/', include(router.urls)),
+    path('status/', views.approvereject),
+] 
